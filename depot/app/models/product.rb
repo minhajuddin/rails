7a6cff84babb652aa642f399aefc3b7334fffcd7
unknown <Minhajuddin@.(none)>
2009-01-26 01:33:35 +0530

    Created the catalog display -- Minhajuddin

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -12,4 +12,8 @@ class Product < ActiveRecord::Base
 	def price_must_be_atleast_a_cent
 		errors.add(:price, 'should be atleast 0.01') if price.nil? || price < 0.01
 	end
+	
+	def self.find_products_for_sale
+		find(:all, :order => "title")
+	end
 end
