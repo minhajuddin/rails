de103ac9d1f80a4ad9c9b26778770eb4ca1502bb
unknown <Minhajuddin@.(none)>
2009-01-26 03:26:55 +0530

    Code at the end of Task C -- Minhajuddin

--- depot/app/controllers/application.rb
+++ depot/app/controllers/application.rb
@@ -3,8 +3,8 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
-
+  session :session_key => "_depot_session_id"
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '603faccb6dea1c28519f6fb3beae9a7a'
+  protect_from_forgery  :secret => '603faccb6dea1c28519f6fb3beae9a7a'
 end
